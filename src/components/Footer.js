91468ba74453136207b07719d9c52e584d94e86d
wenses penadillo lazares <wenses400@gmail.com>
2021-08-20 15:26:24 -0500

    new portfolio design

---
+++ src/components/Footer.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import styled from 'styled-components';
+import FooterCol from './FooterCol';
+import PText from './PText';
+
+const FooterStyle = styled.div`
+  background-color: var(--deep-dark);
+  padding-top: 10rem;
+  .container {
+    display: flex;
+    gap: 3rem;
+  }
+  .footer__col1 {
+    flex: 2;
+  }
+  .footer__col2,
+  .footer__col3,
+  .footer__col4 {
+    flex: 1;
+  }
+  .footer__col1__title {
+    font-size: 3.5rem;
+    margin-bottom: 1rem;
+  }
+  @media only screen and (max-width: 768px) {
+    .container {
+      flex-direction: column;
+      gap: 0rem;
+      & > div {
+        margin-top: 5rem;
+      }
+    }
+    .footer__col1 .para {
+      max-width: 100%;
+    }
+  }
+`;
+
+export default function Footer() {
+  return (
+    <FooterStyle>
+      <div className="container">
+        <div className="footer__col1">
+          <h1 className="footer__col1__title">Wenses Penadillo</h1>
+          <PText>
+            A freelance developer from Lima, Peru. I always make websites that
+            have unique designs and also has a good performance rate.
+          </PText>
+        </div>
+        <div className="footer__col2">
+          <FooterCol
+            heading="Important Links"
+            links={[
+              {
+                title: 'Home',
+                path: '/',
+                type: 'Link',
+              },
+              {
+                type: 'Link',
+                title: 'About',
+                path: '/about',
+              },
+              {
+                type: 'Link',
+                title: 'Projects',
+                path: '/projects',
+              },
+              {
+                type: 'Link',
+                title: 'Contact',
+                path: '/contact',
+              },
+            ]}
+          />
+        </div>
+        <div className="footer__col3">
+          <FooterCol
+            heading="Contact Info"
+            links={[
+              {
+                title: '+51 926387363',
+                path: 'tel:+51926387363',
+              },
+              {
+                title: 'wpenadillol@uni.pe',
+                path: 'mailto:wpenadillol@uni.pe',
+              },
+            ]}
+          />
+        </div>
+        <div className="footer__col4">
+          <FooterCol
+            heading="Social Links"
+            links={[
+              {
+                title: 'GitHub',
+                path: 'http://github.com',
+              },
+              {
+                title: 'LinkedIn',
+                path: 'http://linkedin.com',
+              },
+              {
+                title: 'Twitter',
+                path: 'http://twitter.com',
+              },
+              {
+                title: 'Facebook',
+                path: 'http://facebook.com',
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </FooterStyle>
+  );
+}
