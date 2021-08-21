7fad7075951c54c4d82aff08e2254af85e3779b4
wenses penadillo lazares <wenses400@gmail.com>
2021-08-21 00:29:02 -0500

    minor changes

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,36 +1,36 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-*{
+  * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
   }
-  :root{
+  :root {
     --dark-bg: #262626;
     --gray-1: #BCB4B4;
-    --deep-dark: #1E1E1E;
     --gray-2: #363636;
+    --deep-dark: #1E1E1E;
     --white : white;
     --black: black;
   }
-  html{
+  html {
     font-size: 10px;
     font-family: 'Roboto Mono';
     background-color: var(--dark-bg);
   }
-  ul,li{
+  ul, li {
     list-style: none;
   }
-  a{
+  a {
     text-decoration: none;
   }
-  img, svg{
+  img, svg {
     width: 100%;
     height: 100%;
     object-fit: cover;
   }
-  button{
+  button {
     outline: none
   }
   .container {
