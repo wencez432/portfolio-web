b627b2200c58727cb0bf5e33d25b717db23458e4
wenses penadillo lazares <wenses400@gmail.com>
2021-09-02 15:01:09 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavMenu from './components/NavMenu';
-import ScrollToTop from './components/ScrollToTop';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
@@ -13,8 +12,10 @@ export default function App() {
     <>
       <Router>
         <NavMenu />
-        <ScrollToTop />
         <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
           <Route path="/about">
             <About />
           </Route>
@@ -23,9 +24,6 @@ export default function App() {
           </Route>
           <Route path="/projects">
             <Projects />
-          </Route>
-          <Route path="/">
-            <Home />
           </Route>
         </Switch>
         <Footer />
